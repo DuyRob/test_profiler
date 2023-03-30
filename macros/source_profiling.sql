482f2438e9a4afbe704f5ad56e6a5ff204145450
Ha Le <ha.lv.joonsolutions@amanotes.com>
2023-03-30 08:34:28 +0000

    update source_profiling macro

--- macros/source_profiling.sql
+++ macros/source_profiling.sql
@@ -1,14 +1,28 @@
-{% macro source_profiling(source_name, table_name,check_na =True, date_incremental=None, condition= None) %}
-  {{ return(adapter.dispatch('source_profiling')(source_name, table_name, check_na, date_incremental, condition)) }}
+{% macro source_profiling(
+    source_name,
+    table_name,
+    check_na=True,
+    date_incremental=None,
+    condition=None
+) %}
+{{
+    return(
+        adapter.dispatch("source_profiling")(
+            source_name, table_name, check_na, date_incremental, condition
+        )
+    )
+}}
 {% endmacro %}
 
-{% macro bigquery__source_profiling(source_name, table_name,check_na, date_incremental, condition) %}
+{% macro bigquery__source_profiling(
+    source_name, table_name, check_na, date_incremental, condition
+) %}
 {% set source_relation = source(source_name, table_name) %}
 {% set columns = adapter.get_columns_in_relation(source_relation) %}
 
-{% if execute %}    
-   
-    {% set create_table_query %}
+{% if execute %}
+
+{% set create_table_query %}
 {% if date_incremental is not none %}
 {% raw %}
 {{ config(
@@ -41,61 +55,74 @@ calculation_cte as (
        '{{column.name}}' as column_name,
        '{{column.dtype}}' as data_type,
         current_date() as profiling_date,
-        COUNT( {{ column.name }}) OVER(PARTITION BY partition_field ) as count, 
-        COUNT(DISTINCT {{ column.name }}) OVER(PARTITION BY partition_field ) as count_distinct,
-        COUNTIF({{ column.name }} IS NULL) OVER(PARTITION BY partition_field) as count_null,
-        IEEE_DIVIDE(COUNTIF({{ column.name }} IS NULL) OVER(PARTITION BY partition_field ), COUNT( {{ column.name }} ) OVER(PARTITION BY partition_field )) as null_ratio, 
-        IEEE_DIVIDE(COUNT(DISTINCT {{ column.name }} ) OVER(PARTITION BY partition_field ), COUNT( {{ column.name }}) OVER(PARTITION BY partition_field )) as distinct_ratio, 
+        COUNT( {{ column.name }}) as count, 
+        COUNT(DISTINCT {{ column.name }}) as count_distinct,
+        COUNTIF({{ column.name }} IS NULL) as count_null,
+        IEEE_DIVIDE(COUNTIF({{ column.name }} IS NULL), COUNT( {{ column.name }} )) as null_ratio, 
+        IEEE_DIVIDE(COUNT(DISTINCT {{ column.name }} ), COUNT( {{ column.name }})) as distinct_ratio, 
         {%if check_na %}
            {%- if column.dtype in ('STRING') -%}
-        COUNTIF( lower({{ column.name }}) in ('nan','n/a','-infinty','na','','-','__','--','_')) OVER(PARTITION BY partition_field) as count_na, 
+        COUNTIF( lower({{ column.name }}) in ('nan','n/a','-infinty','na','','-','__','--','_'))) as count_na, 
            {%- else -%}
         null as count_na,
            {%- endif -%}
         {% endif %}
         {% if column.dtype in ('INT64','FLOAT64','NUMERIC','BIGNUMERIC') %}
-        AVG({{ column.name }}) OVER(PARTITION BY partition_field ) as average,
-        SUM({{ column.name }}) OVER(PARTITION BY partition_field ) as sum,
-        STDDEV(COALESCE ({{ column.name }}, 0 )) OVER(PARTITION BY partition_field ) as  standard_dev,
-        MIN(COALESCE ({{ column.name }}, 0 )) OVER (PARTITION BY partition_field)  as min,
-        PERCENTILE_CONT ({{ column.name }},0.25) OVER(PARTITION BY partition_field ) as quantile_25,
-        PERCENTILE_CONT ({{ column.name }},0.5) OVER(PARTITION BY partition_field ) as quantile_50,
-        PERCENTILE_CONT ({{ column.name }},0.75) OVER(PARTITION BY partition_field )  as quantile_75,
-        PERCENTILE_CONT ({{ column.name }},0.9) OVER(PARTITION BY partition_field )  as quantile_90,
+        AVG({{ column.name }}) as average,
+        SUM({{ column.name }}) as sum,
+        STDDEV({{ column.name }}) as  standard_dev,
+        MIN({{ column.name }}) OVER (PARTITION BY partition_field)  as min,
         MAX ({{ column.name }}) OVER (PARTITION BY partition_field)  as max
         {% else %}
         null as average,                           
         null as sum,
         null as standard_dev,
         null as min,
-        null as quantile_25,
-        null as quantile_50,
-        null as quantile_75,
-        null as quantile_90,
         null as max
         {% endif %}
      from prep 
+     group by 1,2,3
     {{"union all " if not loop.last}}
    
 {% endfor %} 
+),
+
+quantile_cte as (
+    {% for column in columns %} 
+     select distinct
+       '{{column.name}}' as column_name,
+       '{{column.dtype}}' as data_type,
+        PERCENTILE_CONT ({{ column.name }},0.25) OVER(PARTITION BY partition_field ) as quantile_25,
+        PERCENTILE_CONT ({{ column.name }},0.5) OVER(PARTITION BY partition_field ) as quantile_50,
+        PERCENTILE_CONT ({{ column.name }},0.75) OVER(PARTITION BY partition_field )  as quantile_75,
+        PERCENTILE_CONT ({{ column.name }},0.9) OVER(PARTITION BY partition_field )  as quantile_90,
+        from prep
+       {{"union all " if not loop.last}}
+    {% endfor %}
 )
                      
-select  * from calculation_cte             
+select  *,
+{{dbt_utils.generate_surrogate_key (['column_name','data_type','profliling_date']) }} as column_key 
+ from calculation_cte   
+left join quantile_cte on 
+calculation_cte.column_name = quantile_cte.column_name
+and calculation_cte.data_type = quantile_cte.data_type
 {% endset %}
 
-   {{ log(create_table_query, info=True) }}
+{{ log(create_table_query, info=True) }}
 
- {% endif %}
+{% endif %}
 {% endmacro %}
 
 
-
-{% macro snowflake__source_profiling(source_name, table_name, date_incremental, check_na ,condition) %}
+{% macro snowflake__source_profiling(
+    source_name, table_name, date_incremental, check_na, condition
+) %}
 {% set source_relation = source(source_name, table_name) %}
 {% set columns = adapter.get_columns_in_relation(source_relation) %}
-{% if execute %}    
-    {% set create_table_query %}
-          
+{% if execute %}
+{% set create_table_query %}
+
             with prep as (
                 select *                    
                 from {% raw %}{{ source({% endraw %}'{{ source_name }}', '{{ table_name }}'{% raw %}) }}{% endraw %}
@@ -120,7 +147,7 @@ select  * from calculation_cte
                         COUNT_IF({{ column.name }} IS NULL)  as count_null,
                         {% if check_na %}
                              {%- if column.dtype in ('STRING', 'VARCHAR','CHAR','CHARACTER','TEXT') -%}
-                         COUNTIF( lower({{ column.name }}) in ('nan','n/a','-infinty','na','','-','__','--','_')) OVER(PARTITION BY partition_field) as count_na, 
+                         COUNTIF( lower({{ column.name }}) in ('nan','n/a','-infinty','na','','-','__','--','_'))) as count_na, 
                              {%- else -%}
                         null as count_na, 
                              {%- endif -%}
@@ -152,9 +179,8 @@ select  * from calculation_cte
             
                 )
                select  * from calculation_cte                      
-        {% endset %}
-    {{ log(create_table_query, info=True) }}
-    
+{% endset %}
+{{ log(create_table_query, info=True) }}
+
 {% endif %}
 {% endmacro %}
-
